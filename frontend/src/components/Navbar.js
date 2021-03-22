e3eb10ef7552de1f9facecfbd694e3d3c044a295
Beandip <brando.lugo@gmail.com>
2021-03-22 11:28:32 -0500

    Added two links to the navbar home and match

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-import {Navbar, NavbarBrand} from "reactstrap";
+import {Navbar, NavbarBrand,Nav, NavItem, NavLink} from "reactstrap";
 
 function NavBar() {
     return (
         <>
             <Navbar color="light">
+                <Nav>
                 <NavbarBrand href="/">PetFinder</NavbarBrand>
+                <NavItem>
+                    <NavLink href="/">Home</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/">Match</NavLink>
+                </NavItem> 
+                </Nav>
+                
             </Navbar>
         </>
     )
