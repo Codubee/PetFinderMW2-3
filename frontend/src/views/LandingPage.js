acb99701ec9a8d54153596a8b44b329abd850884
Oziel-Sal <oziel.salinas22@gmail.com>
2021-03-25 12:02:23 -0500

    Added team component to landingpage.js

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Team from '../components/team.js';
 
 function LandingPage(){
     return(
         <div>
-            {/* Insert your created components here */}
+            {<Team/>},
             <h1>Landing Page Component</h1>
         </div>
     )
