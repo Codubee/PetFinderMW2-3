237e5afe9124ae89d37f6aac0d1a35d65953f59e
Oziel-Sal <oziel.salinas22@gmail.com>
2021-03-25 12:01:31 -0500

    Updated file team.js

--- frontend/src/components/team.js
+++ frontend/src/components/team.js
@@ -1,18 +1,64 @@
 import React from 'react';
 import {
     Card,CardText, CardBody,
-    CardTitle,
+    CardTitle, Row, Col
 } from 'reactstrap';
 
 function Team(){
     return(
         <div>
-            <Card>
-                <CardBody>
-                    <CardTitle tag = "h5">Card Title</CardTitle>
-                    <CardText>Text Goes Here</CardText>
-                </CardBody>
-            </Card>
+            <Row>
+                <Col sm = "4">
+                <Card>
+                    <CardBody>
+                        <CardTitle tag = "h5">Cristian Ruiz</CardTitle>
+                        <CardText>Major: Software Engineering</CardText>
+                        <CardText>School: The University of Texas at Dallas</CardText>
+                        <CardText>Year: </CardText>
+                    </CardBody>
+                 </Card>
+                </Col>
+                <Col sm = "4">
+                <Card>
+                    <CardBody>
+                        <CardTitle tag = "h5">Brando Lugo</CardTitle>
+                        <CardText>Major: Computer Science</CardText>
+                        <CardText>School: The University of Texas Rio Grande Valley</CardText>
+                        <CardText>Year: 1st year as a graduate student </CardText>
+                    </CardBody>
+                 </Card>
+                </Col>
+                <Col sm = "4">
+                <Card>
+                    <CardBody>
+                        <CardTitle tag = "h5">Kristina Kepic</CardTitle>
+                        <CardText>Major: Computer Science</CardText>
+                        <CardText>School: The University of Texas at Dallas</CardText>
+                        <CardText>Year: </CardText>
+                    </CardBody>
+                 </Card>
+                </Col>
+                <Col sm = "4">
+                <Card>
+                    <CardBody>
+                        <CardTitle tag = "h5">Lesli Perez</CardTitle>
+                        <CardText>Major: Computer Engineering</CardText>
+                        <CardText>School: The University of Texas Rio Grande Valley</CardText>
+                        <CardText>Year: </CardText>
+                    </CardBody>
+                 </Card>
+                </Col>
+                <Col sm = "4">
+                <Card>
+                    <CardBody>
+                        <CardTitle tag = "h5">Oziel Salinas</CardTitle>
+                        <CardText>Major: Computer Engineering</CardText>
+                        <CardText>School: The University of Texas Rio Grande Valley</CardText>
+                        <CardText>Year: Junior Year</CardText>
+                    </CardBody>
+                 </Card>
+                </Col>
+            </Row>                
         </div>
     );
 }
