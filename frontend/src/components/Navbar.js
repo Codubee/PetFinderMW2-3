6357a07b780434342df85e369b2f5dc6778a1908
codubee <info@codubee.com>
2021-03-25 12:22:13 -0500

    Adjusted spacing of file

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {Navbar, NavbarBrand,Nav, NavItem, NavLink} from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
 
 import '../style/style.css'
 
@@ -9,14 +9,14 @@ function NavBar() {
         <>
             <Navbar className="navbar">
                 <Nav className="nav">
-                <NavbarBrand className="navbrand"><h1>PetFinder</h1></NavbarBrand>
-                <NavItem>
-                    <NavLink href="/" className="navlink"><h2>Home</h2></NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink href="/match/" className="navlink"><h2>Match</h2></NavLink>
-                </NavItem> 
-                </Nav>               
+                    <NavbarBrand className="navbrand"><h1>PetFinder</h1></NavbarBrand>
+                    <NavItem>
+                        <NavLink href="/" className="navlink"><h2>Home</h2></NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="/match/" className="navlink"><h2>Match</h2></NavLink>
+                    </NavItem>
+                </Nav>
             </Navbar>
         </>
     )
