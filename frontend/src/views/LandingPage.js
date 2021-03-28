e8019ff1261fab1eb63183eae13fd178574dc104
kristinakepic <kepickristina@gmail.com>
2021-03-28 18:30:05 -0500

    Adding the explanation component and test

--- frontend/src/views/LandingPage.js
+++ frontend/src/views/LandingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Explanation from '../components/Explanation.js'
 function LandingPage(){
     return(
         <div>
             {/* Insert your created components here */}
+            <Explanation/>
             <h1>Landing Page Component</h1>
         </div>
     )
