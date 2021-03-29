e1ee65b282fa91cb6c5acbde89cad40307c62120
Oziel-Sal <oziel.salinas22@gmail.com>
2021-03-29 17:12:37 -0500

    Updated team.js for cleaner code. Added a class item that allows a loop for cards

--- frontend/src/components/team.js
+++ frontend/src/components/team.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import {
     Card,CardText, CardBody,
-    CardTitle, Row, Col, CardLink, CardImg
-} from 'reactstrap';
+    CardTitle, Row, Col, CardLink, CardImg } from 'reactstrap';
+
+import data from '../jsonData/jsonData_team.json'
 
 import img1 from '../images/team_member1.jpg'
 import img2 from '../images/team_member2.jpg'
@@ -11,81 +12,44 @@ import img3 from '../images/team_member3.jpg'
 import img4 from '../images/team_member4.jpg'
 import img5 from '../images/team_member5.jpg'
 
-
-
 import '../style/team.css'
-function Team(){
-    return(
-        <div>
-            <h2 className = "title">OUR TEAM</h2>
-            <Row>
-                <Col sm = "3" className = "centercard">
-                <Card>
-                <CardImg bottom width = "100%" src = {img4} alt = "Card image camp" className="img"/>
-                <CardBody className = "cardbackground">
-                        <CardTitle className = "cardtitle" tag = "h5">Lesli Perez</CardTitle>
-                        <CardText  className = "cardtext">Major: Computer Engineering</CardText>
-                        <CardText  className = "cardtext">School: The University of Texas Rio Grande Valley</CardText>
-                        <CardText  className = "cardtext">Year: Junior</CardText>
-                        <CardLink className= "link" href = "https://www.linkedin.com/in/lesli-perez/">Linkedin</CardLink>
-                    </CardBody>
-                 </Card>
-                </Col>
 
-                <Col sm = "3" className = "centercard">
-                <Card>
-                <CardImg bottom width = "100%" src = {img5} alt = "Card image camp"/>
-                <CardBody className = "cardbackground">
-                        <CardTitle className = "cardtitle" tag = "h5">Oziel Salinas</CardTitle>
-                        <CardText  className = "cardtext">Major: Computer Engineering</CardText>
-                        <CardText  className = "cardtext">School: The University of Texas Rio Grande Valley</CardText>
-                        <CardText  className = "cardtext">Year: Junior</CardText>
-                        <CardLink className= "link" href = "https://www.linkedin.com/in/oziel-salinas-8a5a5418b/">Linkedin</CardLink>
-                    </CardBody>
-                 </Card>
-                </Col>
+var image = [String(img4),String(img5),String(img2),String(img3),String(img1)];
 
-                <Col sm = "3" className = "centercard">
-                <Card>
-                <CardImg bottom width = "100%" src = {img2} alt = "Card image camp"/>
-                <CardBody className = "cardbackground">
-                        <CardTitle className = "cardtitle" tag = "h5">Brando Lugo</CardTitle>
-                        <CardText  className = "cardtext" >Major: Computer Science</CardText>
-                        <CardText  className = "cardtext">School: The University of Texas Rio Grande Valley</CardText>
-                        <CardText  className = "cardtext">Year: Graduate </CardText>
-                        <CardLink className= "link" href = "https://www.linkedin.com/in/brandolugo/">Linkedin</CardLink>
-                    </CardBody>
-                 </Card>
-                </Col>
+class Team extends React.Component{
+    constructor(props){
+        super(props)
+        console.log(data);
+    }
 
-                <Col sm = "3" className = "centercard">
-                <Card>
-                <CardImg bottom width = "100%" src = {img3} alt = "Card image camp"/>
-                <CardBody className = "cardbackground">
-                        <CardTitle className = "cardtitle" tag = "h5">Kristina Kepic</CardTitle>
-                        <CardText  className = "cardtext">Major: Computer Science</CardText>
-                        <CardText  className = "cardtext">School: The University of Texas at Dallas</CardText>
-                        <CardText  className = "cardtext">Year: Sophmore</CardText>
-                        <CardLink className= "link" href = "https://www.linkedin.com/in/kristina-kepic-4557a21b8/">Linkedin</CardLink>
-                    </CardBody>
-                 </Card>
-                </Col>
-                
-                <Col sm = "3" className = "centercard">
-                <Card>
-                    <CardImg bottom width="100%" src = {img1} alt = "Card image camp"/>
-                    <CardBody className = "cardbackground">
-                        <CardTitle className = "cardtitle" tag = "h5">Cristian Ruiz</CardTitle>
-                        <CardText  className = "cardtext" >Major: Software Engineering</CardText>
-                        <CardText  className = "cardtext" >School: The University of Texas at Dallas</CardText>
-                        <CardText  className = "cardtext" >Year: Junior</CardText>
-                        <CardLink className= "link" href = "https://www.linkedin.com/in/cristian-ruiz-99b2b4173/">Linkedin</CardLink>
-                    </CardBody>
-                 </Card>
-                </Col>
-            </Row>                
-        </div>
-    );
+    render() {
+        return (
+            <div className = "container">
+                <h2 className="title">OUR TEAM</h2>
+                <Row xs ="3">
+                    {data.map((person, index) => (
+                        <div key={index}>
+                            <Col sm="8" className="centercard">
+                                <Card>
+                                    <CardImg bottom width="100%" src={image[index]} alt="Card image camp" className="img" />
+                                    <CardBody className="cardbackground">
+                                        <CardTitle className="cardtitle" tag="h5">{person.personal_data.name}</CardTitle>
+                                        <CardText className="cardtext">Major: {person.personal_data.major}</CardText>
+                                        <CardText className="cardtext"> School: {person.personal_data.school}</CardText>
+                                        <CardText className="cardtext"> Year: {person.personal_data.year}</CardText>
+                                        <CardLink className="link" href={person.personal_data.link}>Linkedin</CardLink>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+                            <br></br>
+                        </div>
+                    )
+
+                    )}
+                </Row>
+            </div>
+        )
+    }
 }
 
 export default Team;
