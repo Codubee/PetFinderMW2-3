08be4c7d91100b21092bac2f04b5ad78ac4d7b20
Beandip <brando.lugo@gmail.com>
2021-03-29 17:22:41 -0500

    Added ca collapsible feature to the navbar which reacts to the window resize

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler,
+} from "reactstrap";
 
-import '../style/style.css'
+import "../style/style.css";
 
 function NavBar() {
-    return (
-        <>
-            <Navbar className="navbar">
-                <Nav className="nav">
-                    <NavbarBrand className="navbrand"><h1>PetFinder</h1></NavbarBrand>
-                    <NavItem>
-                        <NavLink href="/" className="navlink"><h2>Home</h2></NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/match/" className="navlink"><h2>Match</h2></NavLink>
-                    </NavItem>
-                </Nav>
-            </Navbar>
-        </>
-    )
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar className="navbar" dark expand="md">
+        <NavbarBrand className="navbrand">
+          <h1>PetFinder</h1>
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="nav" navbar>
+            <NavItem>
+              <NavLink href="/" className="navlink">
+                <h2>Home</h2>
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/match/" className="navlink">
+                <h2>Match</h2>
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </>
+  );
 }
 
 export default NavBar;
