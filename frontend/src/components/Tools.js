79e3bac8d3cff12eca624653bf1827b6d5de90dc
crissr11 <43736409+crissr11@users.noreply.github.com>
2021-03-30 15:33:15 -0500

    reduced lines of code in Tools.js

--- frontend/src/components/Tools.js
+++ frontend/src/components/Tools.js
@@ -4,70 +4,39 @@ import {
   } from 'reactstrap'
     
 import '../style/Tools.css'
-import Logo from "../images/React_logo.jpg";
+import Logo1 from "../images/React_logo.jpg";
 import Logo2 from "../images/NodeJS_logo.png";
 import Logo3 from "../images/Jira_logo.jpg";
 import Logo4 from "../images/Postman_logo.jpg";
 import Logo5 from "../images/AWS_logo.jpg";
 import Logo6 from "../images/Heroku_logo.jpg";
 
-function Tools() {
+class Tools extends React.Component {
     
-    return (
-        <div>
-            <h2 className="maintitle">Tools Used:</h2>
+    render(){
+
+        const logo = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6]
+        const titles = ["React", "NodeJS", "Jira", "Postman", "AWS", "Heroku"]
+    
+        return (
+            <div>
+                <h2 className="maintitle">Tools Used:</h2>
                 <Row>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">React</CardTitle>
-                        </CardBody>
-                        <img src={Logo} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">NodeJS</CardTitle>
-                        </CardBody>
-                        <img src={Logo2} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">Jira</CardTitle>
-                        </CardBody>
-                        <img src={Logo3} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">Postman</CardTitle>
-                        </CardBody>
-                        <img src={Logo4} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">AWS</CardTitle>
-                        </CardBody>
-                        <img src={Logo5} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
-                <Col sm="2">
-                    <Card>
-                        <CardBody className="cardbackground">
-                        <CardTitle className="cardtitle" tag="h5">Heroku</CardTitle>
-                        </CardBody>
-                        <img src={Logo6} alt="Card image cap" className="centerlogo"/>
-                    </Card>
-                </Col>
+                    
+                    {titles.map((title, index) =>   
+                        <Col sm="2">
+                            <Card>
+                                <CardBody className="cardbackground">
+                                    <CardTitle className="cardtitle" tag="h5">{title}</CardTitle>
+                                </CardBody>
+                                <img src={logo[index]} alt="Card place" className="centerlogo"/>
+                            </Card>
+                        </Col> 
+                    )}
                 </Row>
-        </div>
-    )
+            </div>
+        )
+    }
 }
 
 export default Tools;
