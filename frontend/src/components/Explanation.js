5b09a56f1d81a0e964abceb51c577708eeae5f5a
kristinakepic <kepickristina@gmail.com>
2021-03-31 14:12:35 -0500

    Changed class to className

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -3,10 +3,10 @@ import '../style/Explanation.css'
 function Explanation(props)
 {
     return(
-    <div class="jumbotron">
-     <div class="container">
-        <h1 class="header">Pet Finder</h1>
-        <h2 class="lead">Looking for a new pet? Then this is the spot for you! Hit the Yes button to match with
+    <div className="jumbotron">
+     <div className="container">
+        <h1 className="header">Pet Finder</h1>
+        <h2 className="lead">Looking for a new pet? Then this is the spot for you! Hit the Yes button to match with
         a pet you like, and no to skip the pet. Once you've matched with some pets you can check out a list of 
         your potential furry friends to find some adoption info.</h2>
     </div>
