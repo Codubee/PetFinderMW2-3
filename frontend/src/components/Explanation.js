7eecdcbb49aefd41d4a82dde58987ec83fc5e564
kristinakepic <kepickristina@gmail.com>
2021-04-05 14:37:01 -0500

    Fixed a typo

--- frontend/src/components/Explanation.js
+++ frontend/src/components/Explanation.js
@@ -7,7 +7,7 @@ function Explanation(props)
      <div className="container">
         <h1 className="header">Pet Finder</h1>
         <h2 className="lead">Looking for a new pet? Then this is the spot for you! Hit the Yes button to match with
-        a pet you like, and no to skip the pet. Once you've matched with some pets you can check out a list of 
+        a pet you like, and No to skip the pet. Once you've matched with some pets you can check out a list of 
         your potential furry friends to find some adoption info.</h2>
     </div>
     </div>
