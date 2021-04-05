b3b5b1c21332a94b0d8fe80e41d8b306a3423766
kristinakepic <kepickristina@gmail.com>
2021-04-05 14:35:22 -0500

    Add match page info from other branches

---
+++ frontend/src/views/MatchPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import FadeIn from "react-fade-in";
+
+function MatchPage() {
+  return (
+    <FadeIn>
+      <div>
+        {/* Insert your created components here */}
+        <h1>Match Page Component</h1>
+      </div>
+    </FadeIn>
+  );
+}
+
+export default MatchPage;
