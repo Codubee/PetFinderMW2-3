1a3e559c9022bda19cd5611cb4f6963a9e0bae4a
Oziel-Sal <oziel.salinas22@gmail.com>
2021-04-06 15:47:25 -0500

    adjusted the formatting

--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 import {
-    Card,CardText, CardBody,
-    CardTitle, Row, Col, CardLink, CardImg, Container} from 'reactstrap';
+    Card, CardText, CardBody,
+    CardTitle, Row, Col, CardLink, CardImg, Container
+} from 'reactstrap';
 
 import data from '../jsonData/jsonData_team.json'
 
@@ -14,27 +15,27 @@ import img5 from '../images/team_member5.jpg'
 
 import '../style/Team.css'
 
-const image = [img4,img5,img2,img3,img1];
+const image = [img4, img5, img2, img3, img1];
 
-class Team extends React.Component{
+class Team extends React.Component {
     render() {
         return (
-            <Container className = "teamcontainer">
+            <Container className="teamcontainer">
                 <h2 className="head_title">OUR TEAM</h2>
                 <Row>
                     {data.map((person, index) => (
-                            <Col sm="12" md = "4"key = {index}>
-                                <Card>
-                                    <CardImg bottom width="100%" src={image[index]} alt="Card image camp" className="img" />
-                                    <CardBody className="teambackground">
-                                        <CardTitle className="teamtitle" tag="h5">{person.personal_data.name}</CardTitle>
-                                        <CardText className="teamtext">{person.personal_data.major}</CardText>
-                                        <CardText className="teamtext">{person.personal_data.school}</CardText>
-                                        <CardText className="teamtext">{person.personal_data.year}</CardText>
-                                        <CardLink className="teamlink" href={person.personal_data.link}>Linkedin</CardLink>
-                                    </CardBody>
-                                </Card>
-                            </Col>
+                        <Col sm="12" md="4" key={index}>
+                            <Card>
+                                <CardImg bottom width="100%" src={image[index]} alt="Card image camp" className="img" />
+                                <CardBody className="teambackground">
+                                    <CardTitle className="teamtitle" tag="h5">{person.personal_data.name}</CardTitle>
+                                    <CardText className="teamtext">{person.personal_data.major}</CardText>
+                                    <CardText className="teamtext">{person.personal_data.school}</CardText>
+                                    <CardText className="teamtext">{person.personal_data.year}</CardText>
+                                    <CardLink className="teamlink" href={person.personal_data.link}>Linkedin</CardLink>
+                                </CardBody>
+                            </Card>
+                        </Col>
                     )
 
                     )}
