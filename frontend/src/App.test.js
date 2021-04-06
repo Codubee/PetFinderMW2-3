ff09ae4240af4b000595db634c933ab4fdb72c4e
Oziel-Sal <oziel.salinas22@gmail.com>
2021-04-06 15:54:08 -0500

    fixed testing issue on App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,8 @@
-/*import { render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  // render(<App />);
-  // const linkElement = screen.getByText('Hello world');
-  // expect(linkElement).toBeInTheDocument();
+   render(<App />);
+   const linkElement = screen.getByText('PetFinder');
+   expect(linkElement).toBeInTheDocument();
 });
-*/
