67d141b10152bea8b4a2da95d2faaef5577e0c5b
Oziel-Sal <oziel.salinas22@gmail.com>
2021-04-11 04:26:40 -0500

    Added buttons to the matching page

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -1,12 +1,24 @@
 import React from "react";
 import FadeIn from "react-fade-in";
+import {ButtonToggle, Container } from "reactstrap";
+import {FaHeart, FaTimes} from 'react-icons/fa'
+
+import '../style/Buttons.css'
 
 function MatchPage() {
   return (
     <FadeIn>
       <div>
-        {/* Insert your created components here */}
-        <h1>Match Page Component</h1>
+        <Container >
+          <ButtonToggle color="white">
+            <FaHeart className="button_design" />
+          </ButtonToggle>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <ButtonToggle color="white">
+            <FaTimes className="button_design" />
+          </ButtonToggle>
+        </Container>
+
       </div>
     </FadeIn>
   );
