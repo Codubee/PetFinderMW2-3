26e304c4602d66f260c00d8af85f2ab7f594da9f
Oziel-Sal <oziel.salinas22@gmail.com>
2021-04-15 13:15:08 -0500

    made minor changes to AnimalDescription.js to remove compile warnings

--- frontend/src/components/AnimalDescription.js
+++ frontend/src/components/AnimalDescription.js
@@ -4,10 +4,6 @@ import '../style/AnimalDescription.css'
 
 class AnimalDescription extends React.Component {
 
-    constructor(props) {
-        super(props)
-    }
-
     render() {
         return (
             <Container className="animalDescriptionContainer">
@@ -16,7 +12,7 @@ class AnimalDescription extends React.Component {
                     <h2 className="descriptionTitle">Animal type:  {this.props.type}</h2>
                     <h2 className="descriptionTitle">Animal sex:   {this.props.sex}</h2>
                     <h2 className="descriptionTitle">Shelter name: {this.props.shelter_name}</h2>
-                    <h2 className="descriptionTitle"><a target="_blank" href={this.props.adoption_url}>Adoption URL</a></h2>
+                    <h2 className="descriptionTitle"><a target="_blank" href={this.props.adoption_url} rel="noreferrer">Adoption URL</a></h2>
                 </div>
             </Container>
         )
