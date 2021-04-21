4d86c71de901acd15c247de825de363c99cf8b24
Beandip <brando.lugo@gmail.com>
2021-04-20 22:54:44 -0500

    Added the get request function /getAnimalDescription

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,45 @@
-const express = require('express')
-const app = express()
-const axios = require('axios');
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const { response } = require("express");
 app.use(express.json());
-
 
 /*
     I will leave this here as an example that you can use
     when building your APIs. I suggest that you try to understand
     what each line is doing. Reach out if you have any questions.
 */
-app.get('/exampleApi', function (req, res) {
-
-    axios.get('https://api.agify.io?name=Mike')
+app.get("/exampleApi", function (req, res) {
+  axios
+    .get("https://api.agify.io?name=Mike")
     .then(function (response) {
-        // handle success and send back a 200 response with the data
-        console.log(response.data);
-        res.status(200).json(response.data);
+      // handle success and send back a 200 response with the data
+      console.log(response.data);
+      res.status(200).json(response.data);
     })
     .catch(function (error) {
-        /* 
+      /* 
             It is good practice to handle the error and send back the 
             error message that was received from the API call
-        */ 
-        console.log(error)
-        res.status(400).json({error:"An error occurred"});
-    })
-})
+        */
+      console.log(error);
+      res.status(400).json({ error: "An error occurred" });
+    });
+});
 
-app.listen(8080, () => console.log('Listening at locahost:8080'))
+app.get("/getAnimalDescription", function (req, res) {
+  axios
+    .get(
+      "https://codubee-projects-api.herokuapp.com/animal/getAnimalDescription"
+    )
+    .then((herokuResponse) => {
+      console.log(herokuResponse.data);
+      res.status(200).json(herokuResponse.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      res.status(400).json({ error: "Oops! An error occured" });
+    });
+});
+
+app.listen(8080, () => console.log("Listening at locahost:8080"));
