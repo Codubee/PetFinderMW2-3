eaee3a5470a720cc0f10e40637f01a8711abc6bf
Beandip <brando.lugo@gmail.com>
2021-04-21 10:11:18 -0500

    Fixed another merging issue or issues

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -2,24 +2,16 @@ import React from "react";
 import FadeIn from "react-fade-in";
 import PhotoDisplay from "../components/PhotoDisplay";
 
-<<<<<<< HEAD
 import animalImg from "../images/DisplayTest.jpg";
 import AnimalDescription from "../components/AnimalDescription";
 import { Button, Container } from "reactstrap";
 import { FaHeart, FaTimes } from "react-icons/fa";
-=======
-import animalImg from "../images/DisplayTest.jpg"
-import AnimalDescription from '../components/AnimalDescription';
-import {Button, Container } from "reactstrap";
-import {FaHeart, FaTimes} from 'react-icons/fa';
->>>>>>> 9db176f6b268d490de7210118584aa29aab3322a
 
 import "../style/Buttons.css";
 
 function MatchPage() {
   return (
     <FadeIn>
-<<<<<<< HEAD
       <Container>
         <PhotoDisplay animalImg={animalImg} alt={"Image failed to load"} />
         <AnimalDescription />
@@ -31,19 +23,6 @@ function MatchPage() {
           <FaTimes className="button_design" title="No" />
         </Button>
       </Container>
-=======
-        <Container >
-          <PhotoDisplay animalImg={animalImg} alt={"Image failed to load"}/>
-          <AnimalDescription />
-          <Button color="">
-            <FaHeart className="button_design" title= "Yes"/>
-          </Button>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Button color="">
-            <FaTimes className="button_design" title= "No" />
-          </Button>
-        </Container>
->>>>>>> 9db176f6b268d490de7210118584aa29aab3322a
     </FadeIn>
   );
 }
