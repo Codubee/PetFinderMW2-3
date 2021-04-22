ef9b32adfc30472b8158cbe6cfdf0cd7a4422711
crissr11 <43736409+crissr11@users.noreply.github.com>
2021-04-21 21:13:04 -0500

    initial commit

--- backend/index.js
+++ backend/index.js
@@ -27,4 +27,21 @@ app.get('/exampleApi', function (req, res) {
     })
 })
 
+// To get all matches from a specific user using their id
+app.get('/getMatches', function (req, res) {
+
+    console.log(req.query)
+    let id = req.query.id;
+    axios.get('https://codubee-projects-api.herokuapp.com/animal/getmatches?id='+id)
+    .then(function (response) {
+        console.log(response.data)
+        res.status(200).json(response.data)
+    })
+    .catch(function (error) {
+        console.log(error)
+        res.status(400).json({error:"An error has occurred"})
+    })
+})
+
+
 app.listen(8080, () => console.log('Listening at locahost:8080'))
