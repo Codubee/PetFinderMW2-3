8c5acb784b5202202ac7415170b3f2464c3bd6ed
lesli-dani <72846459+lesli-dani@users.noreply.github.com>
2021-04-22 23:00:22 -0500

    MW-12-Display matches table (#12)
    * added Display Matches component, style sheet, and jsondata for displaying matches
    
    * added Display Matches table to Match Page, changed table title color
    
    * test for DisplayMatches component
    
    Co-authored-by: user.name <email>
    Co-authored-by: codubee <info@codubee.com>

--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -6,6 +6,7 @@ import animalImg from "../images/DisplayTest.jpg"
 import AnimalDescription from '../components/AnimalDescription';
 import {Button, Container } from "reactstrap";
 import {FaHeart, FaTimes} from 'react-icons/fa'
+import DisplayMatches from '../components/DisplayMatches'
 
 import '../style/Buttons.css'
 
@@ -22,6 +23,7 @@ function MatchPage() {
           <Button color="">
             <FaTimes className="button_design" title= "No" />
           </Button>
+          <DisplayMatches/>
         </Container>
     </FadeIn>
 
