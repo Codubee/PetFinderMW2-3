148875d2ecddcf40385c427bc84e7664c274e011
kristinakepic <kepickristina@gmail.com>
2021-05-18 15:10:52 -0500

    Calling the Weather API

--- frontend/src/components/Weather.js
+++ frontend/src/components/Weather.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios'
 
 class Weather extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { weather: '' };
+    }
+
+componentDidMount() {
+    axios.get("/getWeather")
+        .then((response) => {
+            console.log(response.data);
+            this.setState({
+                weather: response.data.weather
+            })
+        })
+}
+
     render(){
         return(
-            <h1>hello world</h1>
+            <h1>Weather: {this.props.weather}</h1>
         )
     }
 }
