875a5450b640c52732d747233f646acf26c4acf4
Beandip <brando.lugo@gmail.com>
2021-06-01 01:13:17 -0500

    Mw 19 call the getanimaldescription api (#19)
    * Added the code which calls the API
    
    * Added the api call getAnimalDescription to the MatchPage
    
    * Added button clicks to get the animal descriptions
    
    * handled error on get matches
    
    Co-authored-by: codubee <info@codubee.com>
    Co-authored-by: Codubee <70549347+Codubee@users.noreply.github.com>

--- frontend/src/components/AnimalDescription.js
+++ frontend/src/components/AnimalDescription.js
@@ -2,21 +2,20 @@ import React from "react";
 import { Container } from "reactstrap";
 import '../style/AnimalDescription.css'
 
-class AnimalDescription extends React.Component {
+function AnimalDescription(props) {
 
-    render() {
-        return (
-            <Container className="animalDescriptionContainer">
-                <div>
-                    <h2 className="descriptionTitle">Animal name:  {this.props.name}</h2>
-                    <h2 className="descriptionTitle">Animal type:  {this.props.type}</h2>
-                    <h2 className="descriptionTitle">Animal sex:   {this.props.sex}</h2>
-                    <h2 className="descriptionTitle">Shelter name: {this.props.shelter_name}</h2>
-                    <h2 className="descriptionTitle"><a target="_blank" href={this.props.adoption_url} rel="noreferrer">Adoption URL</a></h2>
-                </div>
-            </Container>
-        )
-    }
+    return (
+        <Container className="animalDescriptionContainer">
+            <div>
+                <h2 className="descriptionTitle">Animal name:  {props.description.name}</h2>
+                <h2 className="descriptionTitle">Animal type:  {props.description.type}</h2>
+                <h2 className="descriptionTitle">Animal sex:   {props.description.sex}</h2>
+                <h2 className="descriptionTitle">Shelter name: {props.description.shelter_name}</h2>
+                <h2 className="descriptionTitle"><a target="_blank" href={props.description.adoption_url} rel="noreferrer">Adoption URL</a></h2>
+            </div>
+        </Container>
+    )
+    
 }
 
 export default AnimalDescription;
--- frontend/src/views/MatchPage.js
+++ frontend/src/views/MatchPage.js
@@ -7,6 +7,7 @@ import AnimalDescription from '../components/AnimalDescription';
 import { Button, Container } from "reactstrap";
 import { FaHeart, FaTimes } from 'react-icons/fa'
 import DisplayMatches from '../components/DisplayMatches'
+import axios from 'axios'
 
 import "../style/Buttons.css";
 
@@ -14,19 +15,46 @@ class MatchPage extends React.Component {
 
   constructor(props){
     super(props)
-    this.state = {userId:Math.floor(Math.random() * 100) + 1}
+    this.state = {description:{},userId:Math.floor(Math.random() * 100) + 1}
+    this.getMatches = this.getMatches.bind(this)
+    this.yesClick = this.yesClick.bind(this)
+    this.noClick = this.noClick.bind(this)
+  }
+
+  componentDidMount() {
+    this.getMatches();
+  }
+
+  yesClick(){
+    //TODO: add to user
+    this.getMatches();
+  }
+
+  noClick(){
+    this.getMatches();
+  }
+
+  getMatches(){
+    axios.get("/getAnimalDescription")
+    .then((response) => {
+      console.log(response.data); 
+      this.setState({description:response.data})
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
   }
 
   render() {
     return (
       <FadeIn>
         <Container >
-          <PhotoDisplay animalImg={animalImg} alt={"Image failed to load"} />
-          <AnimalDescription />
-          <Button color="">
+          <PhotoDisplay animalImg={this.state.description.image} alt={"Image failed to load"} />
+          <AnimalDescription description={this.state.description}/>
+          <Button color= ""  onClick={this.yesClick}>
             <FaHeart className="button_design" title="Yes" />
           </Button>
-          <Button color="">
+          <Button color= "" onClick={this.noClick}>
             <FaTimes className="button_design" title="No" />
           </Button>
           <DisplayMatches userId={this.state.userId} />
